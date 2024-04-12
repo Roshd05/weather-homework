a46c54f6063da775182f5b919f7c575772cd683b
Roshd05 <166666105+Roshd05@users.noreply.github.com>
2024-04-12 14:28:11 -0400

    adding axios for the search

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
+function updateWeather(response) {
+    let temperatureElement = document.querySelector("#temperature");
+    let temperature = response.data.temperature.current;
+    let cityElement = document.querySelector("#city");
+
+    cityElement.innerHTML = response.data.city;
+    temperature.innerHTML= Math.round(temperature);
+}
+
+function searchCity(city) (
+    let apiKey="840df5b063f365oba39tb282d8c1d0cc";
+    let apiUrl=`https://api.shecodes.io/weather/v1/current?query=$[city}&key={apiKey}&units=metric`;
+    axios.get(apiUrl).then(updateWeather);
+    )
+
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+ 
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
