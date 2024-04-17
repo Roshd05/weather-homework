92f3b6609b315df2f3029031dce6716977a12d77
Roshd05 <166666105+Roshd05@users.noreply.github.com>
2024-04-17 18:27:27 -0400

    fixed dates ,

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ function refreshWeather(response) {
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windSpeedElement = document.querySelector("#wind-speed");
-  let timeElement = document.querySelector("time")
+  let timeElement = document.querySelector("time");
   let date = new Date(response.data.time * 1000);
 
   cityElement.innerHTML = response.data.city;
@@ -17,24 +17,24 @@ function refreshWeather(response) {
 }
 
 function formatDate(date) {
-    let minutes = date.getMinutes();
-    let hours = date.getHours();
-    let days = [
-        "Sunday"
-        "Monday"
-        "Tuesday"
-        "Wednesday"
-        "Thursday"
-        "Friday"
-        "Saturday",
- ] ;
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
 
-    let day = days[date.getDay()];
+  let day = days[date.getDay()];
 
-    if (minutes <10) {
-        minutes = `o${minutes}`;
-    }
-    return `${day} ${hours}:${minutes}`;
+  if (minutes < 10) {
+    minutes = `o${minutes}`;
+  }
+  return `${day} ${hours}:${minutes}`;
 }
 
 function searchCity(city) {
